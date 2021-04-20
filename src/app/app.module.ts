5e6bb91611d6c6c4e64b16444797e61f20fb30c7
ibmabhishek <erren98@gmail.com>
2021-04-20 11:06:17 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { CreateBugComponent } from './create-bug/create-bug.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { UpdateBugComponent } from './update-bug/update-bug.component';
+import { GetBugComponent } from './get-bug/get-bug.component';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CreateBugComponent,
+    HeaderComponent,
+    FooterComponent,
+    UpdateBugComponent,
+    GetBugComponent,
+
+  ],
+  imports: [
+    BrowserModule, HttpClientModule, FormsModule,
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
